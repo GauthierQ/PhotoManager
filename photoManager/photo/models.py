8166713c5f40f29322348bf188ad0361151e4000
Gauthier <gauthier@nahibu.com>
2022-10-26 14:10:07 +0200

    create model

--- photoManager/photo/models.py
+++ photoManager/photo/models.py
@@ -1,2 +1,6 @@
 from django.db import models
 
+class Photo(models.Model):
+    title = models.CharField(max_length=100)
+    name = models.CharField(max_length=100)
+    create_date = models.DateTimeField()
